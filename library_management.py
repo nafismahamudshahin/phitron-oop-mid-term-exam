4727a9a80cf9efa6f96ed54bb2b8f4c17cd521b4
nafismahamudshahin <nafismahamudshahin@gmail.com>
2025-07-03 21:49:10 +0600

    define return_book method for return borrow books

--- library_management.py
+++ library_management.py
@@ -16,9 +16,12 @@ class Book:
         if self.__availability:
             self.__availability = False
         else: print("This book already borrowing")
-
+    def return_book(self):
+        if not self.__availability:
+            self.__availability = True
+        else: print("this book not available in your hoom Right now this book inside my library.")
     
-Book(101,"computer office application","Lutfor Rahaman") 
-Book(102,"computer office application","Lutfor Rahaman") 
-Book(103,"computer office application","Lutfor Rahaman") 
+# Book(101,"computer office application","Lutfor Rahaman") 
+# Book(102,"computer office application","Lutfor Rahaman") 
+# Book(103,"computer office application","Lutfor Rahaman") 
         
